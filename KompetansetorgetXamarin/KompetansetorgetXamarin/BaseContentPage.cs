8dd51db436fb0bc626652dc59f3be1f6a631697b
patsy02 <peterhagane@yahoo.no>
2016-03-31 04:39:50 +0200

    Apply override to contentpage style (currently only to MainPage, more to come)

---
+++ KompetansetorgetXamarin/KompetansetorgetXamarin/BaseContentPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KompetansetorgetXamarin
+{
+    public class BaseContentPage : ContentPage
+    {
+        public BaseContentPage()
+        {
+            var style = (Style)Application.Current.Resources["basePageStyle"];
+            Style = style;
+        }
+    }
+}
