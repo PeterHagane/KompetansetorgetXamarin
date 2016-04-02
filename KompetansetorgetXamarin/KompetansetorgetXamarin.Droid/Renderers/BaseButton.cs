b2efee621748151146ab85c60203383d41443e07
patsy02 <peterhagane@yahoo.no>
2016-04-02 18:48:25 +0200

    Add custom renderer to .droid for border rendering. WIP, need to find way to override and apply padding.

---
+++ KompetansetorgetXamarin/KompetansetorgetXamarin.Droid/Renderers/BaseButton.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using KompetansetorgetXamarin.Droid;
+using KompetansetorgetXamarin;
+using Android.Graphics.Drawables;
+using System.ComponentModel;
+
+
+//This is one of two classes that renders custom buttons for android. (Both WP and ios would need their own custom button renderer) 
+//This is necessary because the button class used by forms has limited capabilities for customisation
+//The other BaseButton.cs is located in the forms project's Renderer folder, which gets its styles from the resource library in App.xaml
+
+
+[assembly: ExportRenderer(typeof(KompetansetorgetXamarin.Controls.BaseButton), typeof(BaseButton))]
+namespace KompetansetorgetXamarin.Droid
+{
+    public class BaseButton : ButtonRenderer
+    {
+        private GradientDrawable _normal, _pressed;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var button = e.NewElement;
+                var buttonGrey = "#FFA6BCC6";
+
+                int paddingPixel = 5;
+
+
+                // Create a drawable for the button's normal state
+                _normal = new Android.Graphics.Drawables.GradientDrawable();
+
+                if (button.BackgroundColor.R == -1.0 && button.BackgroundColor.G == -1.0 && button.BackgroundColor.B == -1.0)
+                    _normal.SetColor(Android.Graphics.Color.ParseColor("#FFFFFFFF"));  //NOTE: THE TWO FIRST ARE ALPHA
+                else
+                    _normal.SetColor(button.BackgroundColor.ToAndroid());
+
+                _normal.SetStroke((int)button.BorderWidth, button.BorderColor.ToAndroid());
+                _normal.SetCornerRadius(button.BorderRadius);
+
+
+                // Create a drawable for the button's pressed state
+                _pressed = new Android.Graphics.Drawables.GradientDrawable();
+                var highlight = Context.ObtainStyledAttributes(new int[] { Android.Resource.Attribute.ColorActivatedHighlight }).GetColor(0, Android.Graphics.Color.ParseColor(buttonGrey));
+                _pressed.SetColor(highlight);
+                _pressed.SetStroke((int)button.BorderWidth, button.BorderColor.ToAndroid());
+                _pressed.SetCornerRadius(button.BorderRadius);
+
+                // Add the drawables to a state list and assign the state list to the button
+                var sld = new StateListDrawable();
+                sld.AddState(new int[] { Android.Resource.Attribute.StatePressed }, _pressed);
+                sld.AddState(new int[] { }, _normal);
+                Control.SetBackgroundDrawable(sld);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            var button = (Xamarin.Forms.Button)sender;
+
+            if (_normal != null && _pressed != null)
+            {
+                if (e.PropertyName == "BorderRadius")
+                {
+                    _normal.SetCornerRadius(button.BorderRadius);
+                    _pressed.SetCornerRadius(button.BorderRadius);
+                }
+                if (e.PropertyName == "BorderWidth" || e.PropertyName == "BorderColor")
+                {
+                    _normal.SetStroke((int)button.BorderWidth, button.BorderColor.ToAndroid());
+                    _pressed.SetStroke((int)button.BorderWidth, button.BorderColor.ToAndroid());
+                }
+            }
+        }
+    }
+}
