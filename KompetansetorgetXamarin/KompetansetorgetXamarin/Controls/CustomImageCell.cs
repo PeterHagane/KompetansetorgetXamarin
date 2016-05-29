96b064bdebf84829696cc931d29fae13f7f113c3
patsy02 <peterhagane@yahoo.no>
2016-05-29 13:28:06 +0200

    Comment out superfluous code in CustomImageCell

--- KompetansetorgetXamarin/KompetansetorgetXamarin/Controls/CustomImageCell.cs
+++ KompetansetorgetXamarin/KompetansetorgetXamarin/Controls/CustomImageCell.cs
@@ -14,29 +14,27 @@ namespace KompetansetorgetXamarin.Controls
 {
     class CustomImageCell : ImageCell
     {
-        public CustomImageCell() {
-            var moreAction = new MenuItem { Text = "Se annonse" };
-            moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
-            moreAction.Clicked += (sender, e) => {
-                Varsel v;
-                var mi = ((MenuItem)sender);
-                v = (Varsel)mi.CommandParameter;
-                Debug.WriteLine("More Context Action clicked: " + mi.CommandParameter);
-            };
+        //public CustomImageCell() {
+        //    var moreAction = new MenuItem { Text = "Se annonse" };
+        //    moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+        //    moreAction.Clicked += (sender, e) => {
+        //        Varsel v;
+        //        var mi = ((MenuItem)sender);
+        //        v = (Varsel)mi.CommandParameter;
+        //        Debug.WriteLine("More Context Action clicked: " + mi.CommandParameter);
+        //    };
 
-            var deleteAction = new MenuItem { Text = "Slett", IsDestructive = true }; // red background
-            deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
-            deleteAction.Clicked += (sender, e) => { //async
-                Varsel v;
-                var mi = ((MenuItem)sender);
-                v = (Varsel)mi.CommandParameter;
-                Debug.WriteLine("Delete Context Action clicked: " + mi.CommandParameter);
-            };
-            // add to the ViewCell's ContextActions property
-            ContextActions.Add(moreAction);
-            ContextActions.Add(deleteAction);
-        }
-
-
+        //    var deleteAction = new MenuItem { Text = "Slett", IsDestructive = true }; // red background
+        //    deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+        //    deleteAction.Clicked += (sender, e) => { //async
+        //        Varsel v;
+        //        var mi = ((MenuItem)sender);
+        //        v = (Varsel)mi.CommandParameter;
+        //        Debug.WriteLine("Delete Context Action clicked: " + mi.CommandParameter);
+        //    };
+        //    // add to the ViewCell's ContextActions property
+        //    ContextActions.Add(moreAction);
+        //    ContextActions.Add(deleteAction);
+        //}
     }
 }
